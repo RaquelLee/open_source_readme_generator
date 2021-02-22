be422113cefe51ff841cc943fd8f2010bf964a7c
rsco <RaquelLee@users.noreply.github.com>
2021-02-22 17:06:03 -0500

    function added to write to readme file

--- index.js
+++ index.js
@@ -71,9 +71,11 @@ const questions = [
     }
 ];
 
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.error(err) : console.log("Success!");
+    }
+)};
 
 // TODO: Create a function to initialize app
 function init() {}
