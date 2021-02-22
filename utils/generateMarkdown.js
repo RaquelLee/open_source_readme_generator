adf376dbac37b8039c9b699b8c7f359f0b6e9045
rsco <RaquelLee@users.noreply.github.com>
2021-02-22 17:21:32 -0500

    new date to return current year for licensing and copyright notices

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,3 @@
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 const generateMarkdown = data =>
   `# ${data.title}
   ${renderLicenseBadge(renderLicenseLink(data.license))}
@@ -70,3 +66,5 @@ const renderLicenseLink = license => {
       return "Unilicense-brightgreen";
   };
 };
+
+const d = new Date();
